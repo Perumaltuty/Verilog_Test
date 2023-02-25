1b9ae358ed77a12db9ee26eaef188c9e690474b3
Perumal A <112106273+Perumaltuty@users.noreply.github.com>
2023-02-25 12:27:41 +0530

    Create d_latch_ff_tb.v

---
+++ 4. D-latch/d_latch_ff_tb.v
@@ -0,0 +1,23 @@
+module d_latch_ff_tb;
+  reg en, d;
+  wire q;
+  d_latch_with_enable DL(
+    .en(en),
+    .d(d),
+    .q(q)
+  );
+  initial begin
+    en = 0;
+    d = 0;
+    #10 en = 1;
+    #10 d = 1;
+    #10 en = 0;
+    #10 d = 0;
+    #10 en = 1;
+    #10 d = 1;
+    #10 $finish;
+  end
+  always @(q) begin
+    $display("q = %b", q);
+  end
+endmodule
